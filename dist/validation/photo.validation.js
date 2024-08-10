23eee44a198af3e04fe9e404b35fd274a5d46fa9
Hirrua <hirrua.silva20@gmail.com>
2024-08-10 20:11:55 -0300

    fix

--- dist/validation/photo.validation.js
+++
@@ -1,9 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { newObj[key] = obj[key]; } } } newObj.default = obj; return newObj; } }var _yup = require('yup'); var yup = _interopRequireWildcard(_yup);
-
-const photoSchema = yup.object().shape({
-    originalname: yup.string().required("Campo não pode estar vazio"),
-    filename: yup.string().required("Campo não pode estar vazio"),
-    animal_id: yup.number().required("Precisa vincular a um animal")
-})
-
-exports. default = photoSchema
